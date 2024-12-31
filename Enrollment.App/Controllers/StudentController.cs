2d17faa0b135875e36c97c584ed713cb9a8dee68
EruelUrsua <eruelursua@gmail.com>
2024-12-31 13:16:51 +0800

    added Students tab

---
+++ Enrollment.App/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Enrollment.App.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

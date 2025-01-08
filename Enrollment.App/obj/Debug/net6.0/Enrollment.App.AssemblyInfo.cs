cba997cf6a16730dc080549fe095a26ad8056da1
EruelUrsua <eruelursua@gmail.com>
2025-01-09 00:21:36 +0800

    Added Student Crud

--- Enrollment.App/obj/Debug/net6.0/Enrollment.App.AssemblyInfo.cs
+++ Enrollment.App/obj/Debug/net6.0/Enrollment.App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Enrollment.App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d17faa0b135875e36c97c584ed713cb9a8dee68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ffc2713759feb5d5e903f361810a62da3ee6fca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Enrollment.App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Enrollment.App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
